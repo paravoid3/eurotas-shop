81a200a849cc34e2e44712308e53d2734f2c3b86
Irakli <paravoid3@gmail.com>
2020-02-19 00:48:33 +0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let colors = document.querySelectorAll(".colors div");
+
+let colorArray = ["#00ffff", "#000000", "#0000ff", "#ff00ff", "#808080", "#008000", "#00ff00", "#800000", "#000080", "#808000", "#800080", "#ff0000", "#c0c0c0", "#008080", "#ffffff", "#ffff00"]
+
+colors.forEach((color, idx) => {
+  color.style.backgroundColor = colorArray[idx];
+});
+
+
+
+// navbar scroll
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
