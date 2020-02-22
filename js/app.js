0965b6414fd403e22d1d6ede1b866178cd41769f
Irakli <paravoid3@gmail.com>
2020-02-22 19:18:36 +0400

    cleaned code order

--- js/app.js
+++ js/app.js
@@ -1,22 +1,18 @@
-let colors = document.querySelectorAll(".colors div");
-
+let colors = document.querySelectorAll(".color-circles");
+let range = document.querySelector("input[type=range]");
+let price = document.querySelector(".price-number");
 let colorArray = ["#00ffff", "#000000", "#0000ff", "#ff00ff", "#808080", "#008000", "#00ff00", "#800000", "#000080", "#808000", "#800080", "#ff0000", "#c0c0c0", "#008080", "#ffffff", "#ffff00"]
 
 colors.forEach((color, idx) => {
   color.style.backgroundColor = colorArray[idx];
 });
 
-let range = document.querySelector("input[type=range]");
-let price = document.querySelector(".price-number");
-
 range.addEventListener('input', () => {
   price.textContent = `$0 - $${range.value}`
 })
 
-
 // navbar scroll effect
   $(document).scroll(function () {
     var $nav = $(".navbar-fixed-top");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    console.log($(this));
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());;
   });
